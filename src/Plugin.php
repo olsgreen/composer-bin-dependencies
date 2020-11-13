54cc8f56e1621ba738ebd07942a50036770e1204
Oliver Green <green2go@gmail.com>
2020-11-13 20:38:36 +0000

    Composer 2 support.
    Add missing plugin methods & allow composer 2 in development.

--- src/Plugin.php
+++ src/Plugin.php
@@ -276,4 +276,20 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             }
         }
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        //
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        //
+    }
 }
